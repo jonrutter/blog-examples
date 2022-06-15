ce7713475c847331bd95e4ece7087d3b94801668
Jon Rutter <contact@jonrutter.io>
2022-06-15 15:15:41 -0400

    Finished tutorial part 3

--- gatsby-mdx-prismjs/src/pages/index.tsx
+++ gatsby-mdx-prismjs/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout';
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
+      <StaticImage src="../images/featured.jpg" alt="The northern lights" />
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </Layout>
   );
